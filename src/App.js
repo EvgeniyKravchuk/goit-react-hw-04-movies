706d470d0d6d3dd3b071d9cc7e19a81bfcef58fb
EvgeniyKravchuk <gekahhh1996@gmail.com>
2021-08-11 13:12:09 +0300

    finally

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 import Header from "./General/Header";
 import HomePage from "./components/HomePage/HomePage";
-import MoviesPage from "./components/MoviesPage/MoviesPage";
-import MovieDetailsPage from "./components/MovieDetailsPage/MovieDetailsPage";
+const MoviesPage = lazy(() =>
+  import(
+    "./components/MoviesPage/MoviesPage" /* webpackChunkName: "MoviesPage" */
+  )
+);
+const MovieDetailsPage = lazy(() =>
+  import(
+    "./components/MovieDetailsPage/MovieDetailsPage" /* webpackChunkName: "MovieDetailsPage" */
+  )
+);
 
 function App() {
   const [response, setResponse] = useState([]);
@@ -15,12 +23,14 @@ function App() {
         <Route path="/" exact>
           <HomePage movies={response} setMovies={setResponse} />
         </Route>
-        <Route path="/movies" exact>
-          <MoviesPage movies={response} setMovies={setResponse} />
-        </Route>
-        <Route path="/movies/:movieId">
-          <MovieDetailsPage movies={response} setMovies={setResponse} />
-        </Route>
+        <Suspense fallback={<div>...Загружаем</div>}>
+          <Route path="/movies" exact>
+            <MoviesPage movies={response} setMovies={setResponse} />
+          </Route>
+          <Route path="/movies/:movieId">
+            <MovieDetailsPage movies={response} setMovies={setResponse} />
+          </Route>
+        </Suspense>
         {/* <Route>
           <HomePage movies={response} setMovies={setResponse} />
         </Route> */}
